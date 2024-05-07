732e0b33bf15d7316c59fceb520b63a2f160694a
avs-ht <151386266+avs-ht@users.noreply.github.com>
2024-05-07 23:09:15 +0300

    hash history

--- webApp/src/App.tsx
+++ webApp/src/App.tsx
@@ -1,9 +1,13 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import {
+	RouterProvider,
+	createHashHistory,
+	createRouter,
+} from '@tanstack/react-router';
 import { useEffect } from 'react';
 
 import { routeTree } from './routeTree.gen';
 
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, history: createHashHistory() });
 declare module '@tanstack/react-router' {
 	interface Register {
 		router: typeof router;
