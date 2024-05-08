e2be951f3b810fec1eba69918ff14e14721efeb5
avsuvorov <artyom.suvorov.shigoto@gmail.com>
2024-05-08 20:03:02 +0300

    react-intersection-observer

--- webApp/src/widgets/Menu/ui/MenuContentItem/index.tsx
+++ webApp/src/widgets/Menu/ui/MenuContentItem/index.tsx
@@ -1,9 +1,12 @@
+import { useInView } from 'react-intersection-observer';
+
 import { MenuPosition } from '../../model/MenuPosition';
 
 import { MenuPositionItem } from './MenuPositionItem';
 
 interface Props {
 	title: string;
+	id: string;
 	dsecription: string;
 	menuPositions: MenuPosition[];
 }
@@ -13,13 +16,22 @@ export const MenuContentItem = ({
 	dsecription,
 	menuPositions,
 }: Props) => {
+	const { ref, inView } = useInView({
+		/* Optional options */
+		threshold: 0,
+	});
+	if (inView) {
+		console.log(1);
+	}
 	return (
 		<div className="">
-			<h3>{title}</h3>
+			<h3 ref={ref}>{title}</h3>
 			<p>{dsecription}</p>
-			{menuPositions.map(menuPosition => (
-				<MenuPositionItem key={menuPosition.id} menuPosition={menuPosition} />
-			))}
+			<div className="">
+				{menuPositions.map(menuPosition => (
+					<MenuPositionItem key={menuPosition.id} menuPosition={menuPosition} />
+				))}
+			</div>
 		</div>
 	);
 };
