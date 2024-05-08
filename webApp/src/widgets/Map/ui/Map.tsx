d969eed9f496807559215eda0269241dba903188
avs-ht <151386266+avs-ht@users.noreply.github.com>
2024-05-08 12:07:46 +0300

    placemark

--- webApp/src/widgets/Map/ui/Map.tsx
+++ webApp/src/widgets/Map/ui/Map.tsx
@@ -1,6 +1,7 @@
 import {
 	FullscreenControl,
 	GeolocationControl,
+	Placemark,
 	RulerControl,
 	SearchControl,
 	TrafficControl,
@@ -13,20 +14,30 @@ import {
 export const Map = () => {
 	return (
 		<div className="mb-5">
-			<YMaps>
-				<YMap
-					className="aspect-square w-full max-w-[420px]"
-					defaultState={{ center: [55.751574, 37.573856], zoom: 9 }}
-				>
-					<FullscreenControl />
-					<GeolocationControl />
-					<SearchControl options={{ float: 'left' }} />
-					<TrafficControl />
-					<TypeSelector />
-					<ZoomControl />
-					<RulerControl />
-				</YMap>
-			</YMaps>
+			<div className="mb-2">
+				<YMaps>
+					<YMap
+						className="aspect-square w-full max-w-[420px]"
+						defaultState={{ center: [55.751574, 37.573856], zoom: 9 }}
+					>
+						<FullscreenControl />
+						<GeolocationControl />
+						<SearchControl options={{ float: 'left' }} />
+						<TrafficControl />
+						<TypeSelector />
+						<ZoomControl />
+						<RulerControl />
+						<Placemark
+							geometry={[55.751574, 37.573856]}
+							options={{
+								iconColor: 'black',
+								preset: 'islands#blackStrechyIcon',
+							}}
+						/>
+					</YMap>
+				</YMaps>
+			</div>
+			<h1 className="font-bold">ул. Тверская</h1>
 		</div>
 	);
 };
