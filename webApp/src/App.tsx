f00a4983d825af7e236f078dfe3367b1365ce68b
avs-ht <151386266+avs-ht@users.noreply.github.com>
2024-05-08 10:37:21 +0300

    test hsl vars

--- webApp/src/App.tsx
+++ webApp/src/App.tsx
@@ -3,6 +3,7 @@ import {
 	createHashHistory,
 	createRouter,
 } from '@tanstack/react-router';
+import convert from 'color-convert';
 import { useEffect } from 'react';
 
 import { routeTree } from './routeTree.gen';
@@ -18,6 +19,11 @@ const tg = window.Telegram.WebApp;
 export const App = () => {
 	useEffect(() => {
 		tg.ready();
+		const [h, s, l] = convert.hex.hsl(tg.themeParams.bg_color || '');
+		document.body.style.setProperty('--tg-theme-bg-color-h', `${h}`);
+		document.body.style.setProperty('--tg-theme-bg-color-s', `${s}`);
+		document.body.style.setProperty('--tg-theme-bg-color-l', `${l}`);
+		console.log(h, s, l);
 	});
 
 	return <RouterProvider router={router} />;
