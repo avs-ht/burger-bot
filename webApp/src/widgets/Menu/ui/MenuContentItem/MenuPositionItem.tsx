cfb46a947c6db020432c3d6489f49dc198c4e5fe
avsuvorov <artyom.suvorov.shigoto@gmail.com>
2024-05-08 19:44:36 +0300

    typescript fuck #1

---
+++ webApp/src/widgets/Menu/ui/MenuContentItem/MenuPositionItem.tsx
@@ -0,0 +1,10 @@
+import { MenuPosition as MenuPositionType } from '../../model/MenuPosition';
+
+export const MenuPositionItem = ({
+	menuPosition,
+}: {
+	menuPosition: MenuPositionType;
+}) => {
+	console.log(menuPosition);
+	return <></>;
+};
--- webApp/src/widgets/Menu/ui/MenuContentItem/index.tsx
+++ webApp/src/widgets/Menu/ui/MenuContentItem/index.tsx
@@ -1,9 +1,25 @@
 import { MenuPosition } from '../../model/MenuPosition';
 
+import { MenuPositionItem } from './MenuPositionItem';
+
 interface Props {
 	title: string;
 	dsecription: string;
 	menuPositions: MenuPosition[];
 }
 
-export const MenuContentItem = () => {};
+export const MenuContentItem = ({
+	title,
+	dsecription,
+	menuPositions,
+}: Props) => {
+	return (
+		<div className="">
+			<h3>{title}</h3>
+			<p>{dsecription}</p>
+			{menuPositions.map(menuPosition => (
+				<MenuPositionItem key={menuPosition.id} menuPosition={menuPosition} />
+			))}
+		</div>
+	);
+};
